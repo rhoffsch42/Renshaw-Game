fecefc2c04396b0da7415da035f45f959895bfa1
rhoffsch42 <rhoffsch@gmail.com>
2024-05-10 06:13:35 +0200

    removed ts and svelte playgroudns, first push nextjs

--- .save/_bundle.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";fetch("https://lvlvllvlvllvlvl.github.io/RePoE/uniques.json").then().then().catch(),console.log("F")})();
--- .save/webpack.config.js
+++
@@ -1,9 +0,0 @@
-var path = require('path');
-
-module.exports = {
-  entry: './html/test.js',
-  output: {
-    path: path.resolve(__dirname),
-    filename: '_bundle.js'
-  }
-};
