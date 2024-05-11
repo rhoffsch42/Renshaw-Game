28332fe8a35306daab9799ed71b4c7077f767ae0
rhoffsch42 <rhoffsch@gmail.com>
2024-05-11 06:06:17 +0200

    chapter 12 done

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body
+        suppressHydrationWarning={true}
+        className={`${inter.className} antialiased`} >
+        {children}
+      </body>
     </html>
   );
 }
